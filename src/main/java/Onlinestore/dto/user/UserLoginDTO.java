b452be69ea97f3a0fe747c12df71ce3eff343a44
Denys Lapshyn <denyslapshyn@gmail.com>
2025-03-01 23:47:49 +0200

    make proper validation for UserLoginDTO and UserRegistrationDTO

--- src/main/java/Onlinestore/dto/user/UserLoginDTO.java
+++ src/main/java/Onlinestore/dto/user/UserLoginDTO.java
@@ -1,11 +1,21 @@
 package Onlinestore.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserLoginDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 32)
     private String password;
 }
--- src/main/java/Onlinestore/dto/user/UserRegistrationDTO.java
+++ src/main/java/Onlinestore/dto/user/UserRegistrationDTO.java
@@ -1,16 +1,46 @@
 package Onlinestore.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRegistrationDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "^(?![- ])([a-zA-Z -]+)(?<![- ])$")
     private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "^(?![- ])([a-zA-Z -]+)(?<![- ])$")
     private String surname;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 32)
     private String password;
+
+    @NotBlank
+    @Size(min = 6, max = 12)
+    @Pattern(regexp = "/d*")
     private String telephoneNumber;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^(?![- ])([a-zA-Z -]+)(?<![- ])$")
     private String country;
+
+    @NotBlank
+    @Size(min = 10, max = 100)
     private String address;
 }

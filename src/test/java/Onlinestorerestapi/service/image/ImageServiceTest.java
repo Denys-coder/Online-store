d75002c981412b3cc40d7f2407808deb7531da66
Denys Lapshyn <denyslapshyn@gmail.com>
2025-05-31 16:35:14 +0300

    Wrote unit tests for ImageService.getImageDTO()

--- src/test/java/Onlinestorerestapi/service/image/ImageServiceTest.java
+++ src/test/java/Onlinestorerestapi/service/image/ImageServiceTest.java
@@ -1,10 +1,16 @@
 package Onlinestorerestapi.service.image;
 
+import Onlinestorerestapi.exception.ApiException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -36,5 +42,55 @@ public class ImageServiceTest {
         // then
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new ImageService(environment));
         assertEquals("Property 'images.directory' is not set.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void getImageDTO_whenAttemptPathTraversal_throwsException() {
+
+        // given
+        String imageName = "../../etc/passwd";
+        String imageDirectory = "src/test/resources/";
+
+        // when
+        when(environment.getProperty("images.directory")).thenReturn(imageDirectory);
+
+        // then
+        ImageService imageService = new ImageService(environment);
+        ApiException apiException = assertThrows(ApiException.class, () -> imageService.getImageDTO(imageName));
+        assertEquals("Path traversal not allowed", apiException.getMessage());
+    }
+
+    @Test
+    void getImageDTO_whenImageNotExists_throwsException() {
+
+        // given
+        String imageName = "notExistingImage.jpg";
+        String imageDirectory = "src/test/resources/";
+
+        // when
+        when(environment.getProperty("images.directory")).thenReturn(imageDirectory);
+
+        // then
+        ImageService imageService = new ImageService(environment);
+        ApiException apiException = assertThrows(ApiException.class, () -> imageService.getImageDTO(imageName));
+        assertEquals("Specified image does not exist: " + imageName, apiException.getMessage());
+    }
+
+    @Test
+    void getImageDTO_successfullyReturnsImageResponseDTO() throws IOException {
+
+        // given
+        String imageName = "image.webp";
+        String imageDirectory = "src/test/resources/";
+        Resource image = new UrlResource(Paths.get(imageDirectory + imageName).toAbsolutePath().toUri());
+        String contentType = "image/webp";
+
+        // when
+        when(environment.getProperty("images.directory")).thenReturn(imageDirectory);
+
+        // then
+        ImageService imageService = new ImageService(environment);
+        assertEquals(image.contentLength(), imageService.getImageDTO(imageName).getImage().contentLength());
+        assertEquals(contentType, imageService.getImageDTO(imageName).getContentType());
     }
 }

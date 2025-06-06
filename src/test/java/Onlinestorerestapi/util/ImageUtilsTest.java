acf13ec997aa14eb0da2df0f62d8c824e38def5c
Denys Lapshyn <denyslapshyn@gmail.com>
2025-06-06 11:37:06 +0300

    Wrote unit tests for ImageUtilsTest.combineLogoAndPictures()

--- src/test/java/Onlinestorerestapi/util/ImageUtilsTest.java
+++ src/test/java/Onlinestorerestapi/util/ImageUtilsTest.java
@@ -64,4 +64,71 @@ public class ImageUtilsTest {
         List<String> result = imageUtils.combineExistingLogoAndPictureNames(item, false, false);
         assertTrue(result.isEmpty());
     }
+
+    @Test
+    void combineLogoAndPictures_includeBothLogoAndPictures() {
+        // given
+        MultipartFile logo = new MockMultipartFile("logo", "logo.png", "image/png", new byte[0]);
+        MultipartFile pic1 = new MockMultipartFile("pic1", "pic1.png", "image/png", new byte[0]);
+        MultipartFile pic2 = new MockMultipartFile("pic2", "pic2.png", "image/png", new byte[0]);
+        List<MultipartFile> pictures = List.of(pic1, pic2);
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(logo, pictures);
+        assertEquals(List.of(logo, pic1, pic2), result);
+    }
+
+    @Test
+    void combineLogoAndPictures_includeOnlyLogo() {
+        // given
+        MultipartFile logo = new MockMultipartFile("logo", "logo.png", "image/png", new byte[0]);
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(logo, null);
+        assertEquals(List.of(logo), result);
+    }
+
+    @Test
+    void combineLogoAndPictures_includeOnlyPictures() {
+        // given
+        MultipartFile pic1 = new MockMultipartFile("pic1", "pic1.png", "image/png", new byte[0]);
+        MultipartFile pic2 = new MockMultipartFile("pic2", "pic2.png", "image/png", new byte[0]);
+        List<MultipartFile> pictures = List.of(pic1, pic2);
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(null, pictures);
+        assertEquals(List.of(pic1, pic2), result);
+    }
+
+    @Test
+    void combineLogoAndPictures_includeNeitherLogoNorPictures() {
+        // given
+        MultipartFile logo = null;
+        List<MultipartFile> pictures = null;
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(logo, pictures);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void combineLogoAndPictures_emptyPicturesListWithLogo() {
+        // given
+        MultipartFile logo = new MockMultipartFile("logo", "logo.png", "image/png", new byte[0]);
+        List<MultipartFile> pictures = List.of();
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(logo, pictures);
+        assertEquals(List.of(logo), result);
+    }
+
+    @Test
+    void combineLogoAndPictures_emptyPicturesListOnly() {
+        // given
+        List<MultipartFile> pictures = List.of();
+
+        // then
+        List<MultipartFile> result = imageUtils.combineLogoAndPictures(null, pictures);
+        assertTrue(result.isEmpty());
+    }
 }
